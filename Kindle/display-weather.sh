07f78a66414e39245e863181980b3e4bea7e2bbb
rafaceschin <rafaceschin@gmail.com>
2015-03-20 19:50:55 -0400

    Fixed Phoenix-Arizona on image

--- Kindle/display-weather.sh
+++ Kindle/display-weather.sh
@@ -5,7 +5,7 @@ cd /mnt/us/kite/unused
 eips -c
 eips -c
 
-if wget http://server-path/weather/weather-script-output.png; then
+if wget http://path/to/server/weather/weather-script-output.png; then
 	rm display-weather.png
 	mv weather-script-output.png display-weather.png
 	eips -g display-weather.png
